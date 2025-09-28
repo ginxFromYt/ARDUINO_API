b71ce3056d92556933d05803fca3745a9b345b3d
GinxSirr <ginxsirr@ginxsirr.ginxsirr.com>
2025-09-28 10:11:58 +0800

    add route service provider

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the [1mhome[0m route for your application.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/dashboard';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     */
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
